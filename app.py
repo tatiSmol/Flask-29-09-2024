18d2a1b731a9b699bdc70fa3e6e676f891487126
tatiSmol <judeclapton@icloud.com>
2024-10-06 13:36:15 +0300

    ML model (linear regression) added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-from processing import process
+
+from processing import process, preprocess
 
 app = Flask(__name__)
 
@@ -9,14 +10,18 @@ def index():
     message = ''
     if request.method == "POST":
         area = request.form.get("area")
-
+        print("done 1")
         try:
             area = float(area)
-            cost = process(float(area))
-            message += f" Стоимость недвижимости {cost}"
+            print("done 2")
         except:
-            message += 'Некорректный ввод.'
-
+            area = 0
+            message += 'Некорректный ввод. Установлено значение по умолчанию. '
+        scaled_area = preprocess(area)
+        print("done 3")
+        cost = process(scaled_area)
+        print("done 4")
+        message += f"Стоимость недвижимости {cost} рублей"
     return render_template("index.html", message=message)
 
 
--- processing.py
+++ processing.py
@@ -1,4 +1,23 @@
-def process(area):
-    cost = 100_000 * area
-    # TODO: интегрировать модель машинного обучения
+import pickle
+import numpy as np
+import sklearn
+
+
+def process(scaled_area):
+    with open("./models/lr_model.pkl", "rb") as f:
+        regressor = pickle.load(f)
+    predicted_cost = regressor.predict(scaled_area)
+
+    with open("./models/scaler_y.pkl", "rb") as f:
+        min_max_scaler_y = pickle.load(f)
+    cost = np.round(min_max_scaler_y.inverse_transform([predicted_cost])[0][0], 2)
+
     return cost
+
+
+def preprocess(area):
+    with open("./models/scaler_x.pkl", "rb") as f:
+        min_max_scaler_x = pickle.load(f)
+
+    scaled_area = min_max_scaler_x.transform([[area]])
+    return scaled_area
