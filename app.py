c9a9af04a19e329799ac20a6396b235c0db164a2
tatiSmol <judeclapton@icloud.com>
2024-10-06 11:49:41 +0300

    not so important changes

--- app.py
+++ app.py
@@ -9,12 +9,13 @@ def index():
     message = ''
     if request.method == "POST":
         area = request.form.get("area")
+
         try:
             area = float(area)
             cost = process(float(area))
             message += f" Стоимость недвижимости {cost}"
         except:
-            message += 'Некорректный ввод: '
+            message += 'Некорректный ввод.'
 
     return render_template("index.html", message=message)
 
